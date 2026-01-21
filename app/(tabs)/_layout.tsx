11f6a1d3a2f875959f4cc48d607e62a9f04505b5
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-01-21 13:18:58 +0530

    Working - tabs => icons - home page icon customization.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,57 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, ImageBackground, Text } from "react-native";
+
+import { officialdoc } from "@/constants/officialdoc";
+
+const IconC = ({ focused, icon, title }: any) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        //source={}
+        className=""
+      >
+        if (title == Home) {
+          <Image 
+          source={icon} 
+          className="size-12 rounded-full mt-10" 
+        />
+        } else {
+          <Image 
+          source={icon} 
+          className="size-12" 
+          />
+        }
+        <Text className="">{title}</Text>
+      </ImageBackground>
+    );
+  } 
+  // else if (focused && title == "Home") {
+  //   return (
+  //     <ImageBackground
+  //       //source={}
+  //       className=""
+  //     >
+  //       <Image 
+  //         source={icon} 
+  //         className="size-12 " 
+  //       />
+  //       <Text className="">{title}</Text>
+  //     </ImageBackground>
+  //   )
+  // } 
+  else {
+    return (
+      <ImageBackground
+        //source={}
+        className=""
+      >
+        <Image source={icon} className="" />
+        <Text className="">{title}</Text>
+      </ImageBackground>
+    );
+  }
+};
 
 export default function _layout() {
   return (
@@ -24,8 +76,14 @@ export default function _layout() {
         name="index"
         options={{
           title: "Home",
-          //tabBarShowLabel: false,
+          tabBarShowLabel: false,
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <IconC 
+              focused={focused} 
+              icon={officialdoc.logo}
+              title="Home"/>
+          ),
         }}
       ></Tabs.Screen>
       <Tabs.Screen
---
+++ constants/officialdoc.ts
@@ -0,0 +1,7 @@
+import logo from "@/assets/oc/logo.jpg";
+import ee from "@/assets/oc/logo.png";
+
+export const officialdoc = {
+  logo,
+  ee,
+};
