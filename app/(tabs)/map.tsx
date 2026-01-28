aa684a26aa5410ea255a22a18c25a660cec5ca55
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-01-28 19:21:32 +0530

    Connecting - Expo Map.

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { AppleMaps, GoogleMaps } from 'expo-maps';
+import { Platform, Text } from 'react-native';
 
 export default function Map() {
-  return (
-    <View>
-      <Text>map</Text>
-    </View>
-  );
+  if (Platform.OS === 'ios') {
+    return <AppleMaps.View style={{ flex: 1 }} />;
+  } else if (Platform.OS === 'android') {
+    return <GoogleMaps.View style={{ flex: 1 }} />;
+  } else {
+    return <Text>Maps are only available on Android and iOS</Text>;
+  }
 }
