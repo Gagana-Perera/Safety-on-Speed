3a07986242786df019cb0262e893c1d353e88a7b
Pugsy <nimsarakarunaratne@gmail.com>
2026-01-29 23:20:49 +0530

    Fixed all buttons. All UI done, except for the white header. tryong to remove it right now

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,30 @@
-import { Stack } from "expo-router";
+{/*import { Stack } from "expo-router";
 import "./global.css";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{headerShown: false,}}>
+    <    Stack screenOptions={{ headerShown: false, }} >
 
       <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
+  );
+}
+*/}
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "./global.css";
+
+export default function RootLayout() {
+  return (
+    <>
+      <StatusBar style="light" translucent />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </>
   );
 }
