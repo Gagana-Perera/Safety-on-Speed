5588c08a8fd0cd989bf84453cc4cf8974fdd63d3
Chamethya Yasodie <k.chamethya@gmail.com>
2026-02-12 18:27:28 +0530

    Make the icons in the navigation bar visible

--- nativewind-env.d.ts
+++ nativewind-env.d.ts
@@ -1,5 +1,12 @@
 /// <reference types="nativewind/types" />
 
+declare module "*.svg" {
+  import type { ComponentType } from "react";
+  import type { SvgProps } from "react-native-svg";
+  const content: ComponentType<SvgProps>;
+  export default content;
+}
+
 declare module "react-native-immediate-phone-call" {
   export default class RNImmediatePhoneCall {
     static immediatePhoneCall(number: string): void;
